357171ba04765f6da3a82a00675b6cade40828c5
Elijah M. Immer <me@eimmer.me>
2024-11-22 19:21:01 -0800

    version bump to 0.1.7

--- flake.nix
+++ flake.nix
@@ -23,7 +23,7 @@
 
         packages.default = pkgs.stdenvNoCC.mkDerivation (finalAttrs: {
           pname = "walrus-bar";
-          version = "0.1.6";
+          version = "0.1.7";
 
           outputs = ["out"];
 
